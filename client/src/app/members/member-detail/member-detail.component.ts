dc4ec2a7f73548a1729c29e27b048d95950fa9b2
DESKTOP-JKUCR97\Artem <budzinski212@o2.pl>
2025-01-29 21:06:00 +0100

    add gallery

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -4,16 +4,18 @@ import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../models/member.model';
 import { CommonModule } from '@angular/common';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [CommonModule, TabsModule],
+  imports: [CommonModule, TabsModule, GalleryModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.scss',
 })
 export class MemberDetailComponent implements OnInit {
   public member?: Member;
+  public images: GalleryItem[] = [];
 
   constructor(
     private readonly memberService: MembersService,
@@ -28,7 +30,12 @@ export class MemberDetailComponent implements OnInit {
     const username = this.route.snapshot.paramMap.get('username');
     if (!username) return;
     this.memberService.getMember(username).subscribe({
-      next: (member) => (this.member = member),
+      next: (member) => {
+        this.member = member;
+        member.photos.map((photo) => {
+          this.images.push(new ImageItem({ src: photo.url, thumb: photo.url }));
+        });
+      },
     });
   }
 }
